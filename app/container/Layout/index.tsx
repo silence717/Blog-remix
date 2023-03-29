77f1a5611d94dfbbc7f0707f4227ee24e719f03b
fang.yang <yangfang2028@126.com>
2023-03-29 20:59:40 +0800

    ✨ Add blog tracking by howxm.

--- app/container/Layout/index.tsx
+++ app/container/Layout/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useHowxm from 'react-use-howxm'
 import { Layout } from '../../component'
 
 const LayoutEntry = () => {
+  const { initHowxm } = useHowxm()
+
+  useEffect(() => {
+    initHowxm('7274c163-0649-4356-8ce2-588c7bf4ead6')
+  }, [])
+
   return <Layout />
 }
 
